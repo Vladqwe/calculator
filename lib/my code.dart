9b67da74361bc1beb89803a688f06067dde7b390
Vladqwe <vladyslav.hryschenko@gmail.com>
2022-01-22 10:33:13 +0200

    test

---
+++ lib/my code.dart
@@ -0,0 +1,35 @@
+
+
+import 'package:calcul/abstract%20class.dart';
+
+class MyCalc extends SimpleCalc{
+  @override
+  double divide(double a, double b) {
+
+      double result = a / b;
+      return result;
+
+  }
+
+  @override
+  double multiply(double a, double b) {
+    // TODO: implement multiply
+    double result = a * b;
+    return result;
+  }
+
+  @override
+  double substract(double a, double b) {
+    // TODO: implement substract
+    double result = a - b;
+    return result;
+  }
+
+  @override
+  double sum(double a, double b) {
+    // TODO: implement sum
+    double result = a + b;
+    return result;
+  }
+
+}
